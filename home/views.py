c94bcfbacb48b41ebdd89ce08b918c1ebb7f0426
Arpitkh <itsarpit20@gmail.com>
2018-07-25 11:20:38 +0530

    Print message added

--- home/views.py
+++ home/views.py
@@ -4,9 +4,11 @@ from wall.settings import BASE_DIR
 
 
 def index(request):
+    print("wassups")
     all_items = Item.objects.all()
     print(BASE_DIR)
     return render(request, 'home/index.html', {'all_items': all_items})
+
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
