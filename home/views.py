362f1962557dc45d0d9b76176a363bbd0dcdb03d
Arpitkh <itsarpit20@gmail.com>
2018-07-27 14:51:49 +0530

    Search Function Added

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 
 def index(request):
     all_items = Item.objects.all()
-    return render(request, 'home/index.html', {'all_items': all_items})
+    return render(request, 'home/index.html', {'reqd_items': all_items})
 
 
 def detail(request, pk):
@@ -12,13 +12,22 @@ def detail(request, pk):
     return render(request, 'home/detail.html', {'item': item})
 
 def search(request):
+    all_items = Item.objects.all()
     searched_item = request.POST.get('textfield')
-    return render(request, 'home/index.html')
+    searched_item_words = searched_item.split()
+    list = []
+    for item in all_items:
+        count = 0
+        item_name = item.item_name
+        for char in item_name:
+            if char in "():,+":
+                item_name = item_name.replace(char, '')
+        temp = item_name.split()
+        for word in temp:
+            for sword in searched_item_words:
+                if word.lower() == sword.lower():
+                    count += 1
+        if count > 0:
+            list.append(item)
 
-
-
-
-
-
-
-
+    return render(request, 'home/index.html', {"reqd_items": list})
